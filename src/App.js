b7be25d605051ab1056a8cc809faa7618a40449b
ianchen0419 <ianchen0419@gmail.com>
2022-07-22 22:31:33 +0800

    add eslint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const para = document.createElement('p');
+  para.textContent = 'This is auto visible';
+  document.getElementById('root').append(para);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
+        <p>123</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,20 @@
-const reportWebVitals = onPerfEntry => {
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    // import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    //   getCLS(onPerfEntry);
+    //   getFID(onPerfEntry);
+    //   getFCP(onPerfEntry);
+    //   getLCP(onPerfEntry);
+    //   getTTFB(onPerfEntry);
+    // });
+
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
